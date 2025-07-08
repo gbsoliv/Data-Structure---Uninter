2f8241d6df23f90ff93f0f494fa84d15b063acd4
Gabriela <gbsoliveira@hotmail.com>
2025-07-08 10:44:42 -0400

    coding insert pacients with no priority function

--- LinkedList/main.py
+++ LinkedList/main.py
@@ -9,18 +9,43 @@ class ListaEncadeadaSimples:
         self.head = None
 
     def inserir(self, card, cardNumber):
-        new = self.Nodo(card, cardNumber)
+        nodo = self.Nodo(card, cardNumber)
         if self.head is None:
-            self.head = new
+            self.head = nodo
 
+        if card == 'A':
+            self.inserirComPrioridade(nodo)
+        else:
+            self.inserirSemPrioridade(nodo)
 
-def inserirSemPrioridade(nodo):
-    # Função para inserir paciente sem prioridade
-    pass
+def inserirSemPrioridade(self, nodo):
+    if self.head is None:
+        self.head = nodo
+    else:
+        nodoAtual = self.head
+        while nodoAtual.proximo is not None:
+            nodoAtual = nodoAtual.proximo
+        nodoAtual.proximo = nodo
+    
 
-def inserirComPrioridade(nodo): 
-    # Função para inserir paciente com prioridade
-    pass
+def inserirComPrioridade(self, nodo):
+    nodoAtual = self.head
+    anterior = None 
+    while nodoAtual is not None:
+        if nodoAtual.card == 'A' and nodo.cardNumber < nodoAtual.cardNumber:
+            break
+        if nodoAtual.card == 'V':
+            break
+        anterior = nodoAtual
+        nodoAtual = nodoAtual.proximo
+
+    if anterior is None:
+        nodo.proximo = self.head
+        self.head = nodo
+    else:
+        nodo.proximo = nodoAtual
+        anterior.proximo = nodo
+
 
 def imprimirListaEspera():
     # Função para imprimir a lista de espera
@@ -32,26 +57,29 @@ def atenderPaciente():
 
 
 # PROGRAMA PRINCIPAL
+fila = ListaEncadeadaSimples()
+
 while True: 
-    option = int(input(print(''' 
+    option = int(input(''' 
       [1] Adicionar paciente a fila.
       [2] Mostrar pacientes na fila.
       [3] Chamar paciente.
       [4] Sair.
       Escolha uma opção: 
-      ''' )))
+      ''' ))
 
     if option == 1:
-        card = input('Informe a cor do cartão do paciente: (A/V) ')
+        card = input('Informe a cor do cartão do paciente: (A/V) ').upper()
         cardNumber = int(input('Informe o número do cartão do paciente: '))
         fila.inserir(card, cardNumber)
+
     elif option == 2:
         print('LISTA DE PACIENTES:')
-        imprimirListaEspera()
-        # Aqui você deve implementar a lógica para mostrar os pacientes na fila de espera.
+        fila.imprimirListaEspera()
+        
     elif option == 3:
-        atenderPaciente()
-        # Aqui você deve implementar a lógica para chamar o paciente da fila de espera.
+        fila.atenderPaciente()
+        
     elif option == 4:
         print('Saindo do sistema...')
         break
